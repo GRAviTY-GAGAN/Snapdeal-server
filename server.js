b1c46409f4818e75d43a3043dbf2d5611ca84efa
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2023-03-29 13:55:22 +0530

    update1

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+/* // server.js
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
@@ -10,4 +10,24 @@ server.use(middlewares);
 server.use("", router);
 server.listen(process.env.PORT || 5000, () => {
   console.log("JSON Server is running");
+});
+ */
+
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = 8000;
+
+server.listen(process.env.PORT || PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
