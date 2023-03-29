9e94b84d76134cfbf21dc969ae436c6759840e82
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2023-03-29 13:50:38 +0530

    update

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('', router)
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const fs = require("fs");
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+const router = jsonServer.router(db);
+
+server.use(middlewares);
+server.use("", router);
 server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
